75f08523f0a0674717ec46df48c370b70eef3afa
rodrigoota <rodrigotety@hotmail.com>
2022-05-06 02:09:32 -0300

    add feat  findByIdConta

--- bankline-api/src/main/java/com/dio/santander/banklineapi/repository/MovimentacaoRepository.java
+++ bankline-api/src/main/java/com/dio/santander/banklineapi/repository/MovimentacaoRepository.java
@@ -1,9 +1,13 @@
 package com.dio.santander.banklineapi.repository;
 
+import java.util.List;
+
 import com.dio.santander.banklineapi.model.Movimentacao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
+
+    public List<Movimentacao> findByIdConta(Integer idConta);
 
 }
